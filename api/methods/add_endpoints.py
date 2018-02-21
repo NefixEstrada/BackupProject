fa5c18e53e903741409ca2d94fae9cb245fc54cd
Néfix Estrada <nefixestrada@gmail.com>
2018-02-21 22:51:48 +0100

    Added endpoint and finished the GET method
    Added the endpoint to make the Backup resource avaliable and finished the GET method. Edited the POST and PUT to permit getting the backup_id

--- api/methods/add_endpoints.py
+++ api/methods/add_endpoints.py
@@ -3,9 +3,11 @@
 
 # Resources imports
 from api.resources.backups_resource import Backups
+from api.resources.backup_resource import Backup
 
 
 # Add all the endpoints to the API
 def add_endpoints(api):
     root = "/api"
     api.add_resource(Backups, f"{root}/backups")
+    api.add_resource(Backup , f"{root}/backup/<backup_id>")
--- api/resources/backup_resource.py
+++ api/resources/backup_resource.py
@@ -2,28 +2,38 @@
 # -*- coding: utf-8 -*-
 
 # Imports
+import json
 from flask_restful import Resource, reqparse
+from api.methods.files_methods import read_backups
+from api.methods.commands_methods import get_output
 
 
 # Backup resource
 class Backup(Resource):
 
     # GET
-    def get(self):
+    def get(self, backup_id):
         """
         Gets a list of all the archives of a specific backup
         """
-        pass
+        backups = read_backups()
+        for backup in backups:
+            if backup["id"] == backup_id:
+                backup_path = backup["path"]
+
+        archives = json.loads(get_output(f"borg list --json {backup_path}"))["archives"]
+
+        return {"archives": archives}, 200
 
     # POST
-    def post(self):
+    def post(self, backup_id):
         """
         Creates a new archive based on the paths specified in the settings.json file
         """
         pass
 
     # PUT
-    def put(self):
+    def put(self, backup_id):
         """
         Edits the backup name or / and the paths to create the backup
         """
