420bbaaeaf1aa81e1cd3599c4793e8806e467043
Néfix Estrada <nefixestrada@gmail.com>
2018-02-22 18:57:32 +0100

    Fixed the string normalization
    Now it normalizes the strings correctly. Thanks to my M04 teacher for helping me in that :D

--- api/methods/normalization_methods.py
+++ api/methods/normalization_methods.py
@@ -5,15 +5,29 @@
 import unicodedata
 import string as string_library
 
+
 # Normalize string
-# TODO: - Better normalizer
 def normalize_string(string):
+    print(string)
     # Remove accents and non UTF-8 strings
-    string = ''.join(letter for letter in unicodedata.normalize("NFKD", string) if letter in string_library.ascii_letters or letter == " ")
-    characters_to_avoid = "|/&;:{}\$%#@= "
-    for character in characters_to_avoid.split():
-        string = string.replace(character, "_")
-    return string.lower()
+    string = "".join(letter for letter in unicodedata.normalize("NFKD", string) if letter in string_library.ascii_letters or letter == " ")
+    string = string.replace(" ", "_")
+
+    # Remove consecutive _'s
+    new_string = []
+    for number in range(len(string)):
+        current_character = string[number]
+        if not(number != 0 and current_character == string[number - 1] and current_character == "_"):
+            new_string.append(current_character)
+    new_string = "".join(new_string)
+
+    if new_string[0] == "_":
+        new_string = new_string[1:]
+
+    elif new_string[-1] == "_":
+        new_string = new_string[:-1]
+
+    return new_string
 
 
 # Normalize parser
