914773fc78548ac5d86b85bb28f5493385bb53ed
Néfix Estrada <nefixestrada@gmail.com>
2018-03-06 07:25:25 +0100

    Added a method and improved the POST
    The new method makes the code more DRY. Also, the POST method now checks if there's already an archive with the name passed and if it is, it returns a nice error

--- api/methods/backups_methods.py
+++ api/methods/backups_methods.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # Imports
+import json
 from api.methods.files_methods import read_backups
+from api.methods.commands_methods import get_output
 
 
 # Get a backup by id
@@ -11,3 +13,11 @@ def get_backup(backup_id):
     for backup in backups:
         if backup["id"] == backup_id:
             return backup
+
+
+# Get a list of archives by backup id
+def get_archives(backup_id):
+    backup_path = get_backup(backup_id)["path"]
+    archives = json.loads(get_output(f"borg list --json {backup_path}"))["archives"]
+
+    return archives
