ee5e05f26bf0b3b68ee85f7941af26320dc4753c
Néfix Estrada <nefixestrada@gmail.com>
2018-03-07 22:44:35 +0100

    Added the new view to the router and created a simple
    This is simple stuff, needs to have more fancy styling

---
+++ website/src/views/Archive.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="archive">
+    <p v-for="item in content" :key="item">{{ item.path }}</p>
+  </div>
+</template>
+
+<script>
+import { myApi } from '../api.js'
+
+export default {
+  name: 'archive',
+  data () {
+    return {
+      content: []
+    }
+  },
+  created: function () {
+    this.getArchive()
+  },
+  methods: {
+    getArchive: function () {
+      myApi.get(`backup/${this.$route.params.backupId}/${this.$route.params.archiveName}`).then((res) => {        
+        this.content = res.data.content
+      })
+    }
+  }
+
+}
+</script>
